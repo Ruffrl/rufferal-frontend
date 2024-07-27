a6a14b42f75cf32246fd04eaaea3edd9abefdd80
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-07-27 11:07:27 -0500

    Begin testing form input and login endpoints

--- shared/store/src/lib/app.tsx
+++ shared/store/src/lib/app.tsx
@@ -1,18 +1,17 @@
-import { useState } from 'react';
+// import { useRef, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { GetUser } from './components/tester/GetUser';
-import { GetUsers } from './components/tester/GetUsers';
+import { RLogInForm } from './rufferal-design/system';
 
 export const App = () => {
-  const [data, setData] = useState([]);
-  const [authToken, setAuthToken] = useState<null | string>('');
-  const [loading, setLoading] = useState(true);
-  const [singleUserLoading, setSingleUserLoading] = useState(true);
-  const [singleUserError, setSingleUserError] = useState<unknown>();
-  const [singleUserData, setSingleUserData] = useState();
-  const [signUpLoading, setSignUpLoading] = useState(true);
-  const [signUpError, setSignUpError] = useState<unknown>();
-  const [signUpData, setSignUpData] = useState();
+  // const [data, setData] = useState([]);
+  // const [authToken, setAuthToken] = useState<null | string>('');
+  // const [loading, setLoading] = useState(true);
+  // const [singleUserLoading, setSingleUserLoading] = useState(true);
+  // const [singleUserError, setSingleUserError] = useState<unknown>();
+  // const [singleUserData, setSingleUserData] = useState();
+  // const [signUpLoading, setSignUpLoading] = useState(true);
+  // const [signUpError, setSignUpError] = useState<unknown>();
+  // const [signUpData, setSignUpData] = useState();
 
   // const handleSingleUser = async () => {
   //   setSingleUserLoading(true);
@@ -72,117 +71,119 @@ export const App = () => {
   // console.log('signUpData', signUpData);
   // console.log('signUpError', signUpError);
 
-  const signUp = () => {
-    fetch('http://localhost:3000/signup', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        user: {
-          email: 'test@test.com',
-          password: '123456',
-          forename: 'Blarg',
-          surname: 'McBlargson',
-        },
-      }),
-    })
-      .then((response) => {
-        const authHeader = response.headers.get('Authorization');
-        setAuthToken(authHeader);
-        return response.json();
-      })
-      .then((json) => {
-        console.log(JSON.stringify(json));
-      })
-      .catch((error) => {
-        console.log('ERROR SIGNUP', error);
-      })
-      .finally();
-  };
-  console.log('authToken', authToken);
+  // const signUp = () => {
+  //   fetch('http://localhost:3000/signup', {
+  //     method: 'POST',
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       user: {
+  //         email: 'test@test.com',
+  //         password: '123456',
+  //         forename: 'Blarg',
+  //         surname: 'McBlargson',
+  //       },
+  //     }),
+  //   })
+  //     .then((response) => {
+  //       const authHeader = response.headers.get('Authorization');
+  //       setAuthToken(authHeader);
+  //       return response.json();
+  //     })
+  //     .then((json) => {
+  //       console.log(JSON.stringify(json));
+  //     })
+  //     .catch((error) => {
+  //       console.log('ERROR SIGNUP', error);
+  //     })
+  //     .finally();
+  // };
+  // console.log('authToken', authToken);
 
-  const logIn = () => {
-    fetch('http://localhost:3000/login', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        user: {
-          email: 'test@test.com',
-          password: '123456',
-        },
-      }),
-    })
-      // .then((response) => {
-      //   console.log('BUFFFFFF');
-      //   for (var pair of Object.entries(response.headers)) {
-      //     console.log('BLARG');
-      //     console.log(pair[0] + ': ' + pair[1]);
-      //     console.log('POOP');
-      //     if (pair[0] === 'Authorization') {
-      //       setAuthToken(pair[1]);
-      //     }
-      //   }
-      //   return response.json();
-      // })
-      // .then((json) => {
-      //   for (let pair of Object.entries(json.headers)) {
-      //     console.log('BLARG');
-      //     console.log(pair[0] + ': ' + pair[1]);
-      //     console.log('POOP');
-      //     debugger;
-      //     if (pair[0] === 'Authorization') {
-      //       const blarg = pair[1];
-      //       debugger;
-      //       setAuthToken('jhkjhk');
-      //     }
-      //   }
-      //   setAuthToken(json.headers.get('Authorization'));
-      //   console.log(JSON.stringify({ thisBeTest: 'BIFF' }));
-      //   console.log(JSON.stringify(json));
-      //   console.log('authToken', authToken);
-      // })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(JSON.stringify(json));
-      })
-      .catch((error) => {
-        console.log('ERROR LOGIN', error);
-      })
-      .finally();
-  };
+  // const logIn = () => {
+  //   fetch('http://localhost:3000/login', {
+  //     method: 'POST',
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       user: {
+  //         email: 'test@test.com',
+  //         password: '123456',
+  //       },
+  //     }),
+  //   })
+  //     // .then((response) => {
+  //     //   console.log('BUFFFFFF');
+  //     //   for (var pair of Object.entries(response.headers)) {
+  //     //     console.log('BLARG');
+  //     //     console.log(pair[0] + ': ' + pair[1]);
+  //     //     console.log('POOP');
+  //     //     if (pair[0] === 'Authorization') {
+  //     //       setAuthToken(pair[1]);
+  //     //     }
+  //     //   }
+  //     //   return response.json();
+  //     // })
+  //     // .then((json) => {
+  //     //   for (let pair of Object.entries(json.headers)) {
+  //     //     console.log('BLARG');
+  //     //     console.log(pair[0] + ': ' + pair[1]);
+  //     //     console.log('POOP');
+  //     //     debugger;
+  //     //     if (pair[0] === 'Authorization') {
+  //     //       const blarg = pair[1];
+  //     //       debugger;
+  //     //       setAuthToken('jhkjhk');
+  //     //     }
+  //     //   }
+  //     //   setAuthToken(json.headers.get('Authorization'));
+  //     //   console.log(JSON.stringify({ thisBeTest: 'BIFF' }));
+  //     //   console.log(JSON.stringify(json));
+  //     //   console.log('authToken', authToken);
+  //     // })
+  //     .then((response) => response.json())
+  //     .then((json) => {
+  //       console.log(JSON.stringify(json));
+  //     })
+  //     .catch((error) => {
+  //       console.log('ERROR LOGIN', error);
+  //     })
+  //     .finally();
+  // };
 
-  const logOut = () => {
-    fetch('http://localhost:3000/logout', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        user: {
-          email: 'test@test.com',
-          password: '123456',
-        },
-      }),
-    })
-      .then((response) => response.json())
-      .then((responseData) => {
-        console.log(JSON.stringify(responseData));
-      })
-      .finally();
-  };
+  // const logOut = () => {
+  //   fetch('http://localhost:3000/logout', {
+  //     method: 'POST',
+  //     headers: {
+  //       Accept: 'application/json',
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       user: {
+  //         email: 'test@test.com',
+  //         password: '123456',
+  //       },
+  //     }),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((responseData) => {
+  //       console.log(JSON.stringify(responseData));
+  //     })
+  //     .finally();
+  // };
 
   return (
     <View style={styles.container}>
       <Text style={styles.welcome}>Rufferal</Text>
       <Text style={styles.instructions}>Authentication Playground</Text>
-      <GetUsers />
-      <GetUser />
+
+      <RLogInForm />
+      {/* <GetUsers />
+      <GetUser /> */}
       {/* TESTING SIGNUP (POST) */}
       {/* <View style={styles.buttonPadding}>
         <RButton title="sign up" onPress={() => signUp()} />
