bcf163f8ed7dfb6e9461a98034e314b48f100061
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-08-05 14:57:22 -0500

    Add placeholder for logout behavior

--- shared/store/src/rufferal-design/components/organism/RLogoutButton.tsx
+++ shared/store/src/rufferal-design/components/organism/RLogoutButton.tsx
@@ -1,9 +1,9 @@
 import { observer } from 'mobx-react-lite';
 import * as React from 'react';
 import { useState } from 'react';
-import { Platform, View } from 'react-native';
+import { View } from 'react-native';
 import { observableAuthStore } from '../../../store';
-import { FormErrorProps, RButton, RFormError } from '../atom';
+import { RButton, RFormError } from '../atom';
 
 export const RLogoutButton = observer(
   ({ id }: { id: number }): React.ReactElement => {
@@ -14,39 +14,42 @@ export const RLogoutButton = observer(
     /* BEHAVIORS */
     const handleLogout = async () => {
       // Handle form submission
-      //  DELETE     /sessions/:id
-      const url =
-        Platform.OS === 'android'
-          ? `http://10.0.2.2:5000/sessions/${id}`
-          : `http://localhost:5000/sessions/${id}`;
-
       setLoading(true);
       setError('');
+      console.log(`Logging out user ${id}`);
+      observableAuthStore.revokeAuth();
+      setLoading(false);
+      
+      //  DELETE     /sessions/:id
+      // BLARG - after I update backend with access and refresh tokens, a whitelist, and new behavior for how to handle login and logout - make changes
+      // const url =
+      //   Platform.OS === 'android'
+      //     ? `http://10.0.2.2:5000/sessions/${id}`
+      //     : `http://localhost:5000/sessions/${id}`;
+      // try {
+      //   const response = await fetch(url, {
+      //     method: 'DELETE',
+      //     headers: {
+      //       Accept: 'application/json',
+      //       'Content-Type': 'application/json',
+      //       Authorization: String(observableAuthStore.bearerToken),
+      //     },
+      //   });
 
-      try {
-        const response = await fetch(url, {
-          method: 'DELETE',
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-            Authorization: String(observableAuthStore.bearerToken),
-          },
-        });
+      //   if (!response.ok) {
+      //     const error: FormErrorProps = await response.json();
+      //     throw new Error(error.error);
+      //   }
 
-        if (!response.ok) {
-          const error: FormErrorProps = await response.json();
-          throw new Error(error.error);
-        }
-
-        const result = await response.json();
-        observableAuthStore.revokeAuth();
-        console.log('result is: ', JSON.stringify(result, null, 4));
-        // BLARG NAVIGATE
-      } catch (err) {
-        setError(String(err));
-      } finally {
-        setLoading(false);
-      }
+      //   const result = await response.json();
+      //   observableAuthStore.revokeAuth();
+      //   console.log('result is: ', JSON.stringify(result, null, 4));
+      //   // BLARG NAVIGATE
+      // } catch (err) {
+      //   setError(String(err));
+      // } finally {
+      //   setLoading(false);
+      // }
     };
 
     return (
