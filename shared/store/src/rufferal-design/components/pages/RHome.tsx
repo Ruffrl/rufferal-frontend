41b73df5a29708573dc14559e07e43bbe846981a
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-08-05 16:20:10 -0500

    Add and test `AsyncStorage`
    - https://react-native-async-storage.github.io/async-storage/
    - https://github.com/react-native-async-storage/async-storage

--- shared/store/src/rufferal-design/components/pages/RHome.tsx
+++ shared/store/src/rufferal-design/components/pages/RHome.tsx
@@ -1,4 +1,6 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { observer } from 'mobx-react-lite';
+import { useEffect, useState } from 'react';
 import { Text } from 'react-native';
 import { RFormLogin } from '../organism';
 import { RAccessTemplate } from '../templates';
@@ -9,6 +11,63 @@ export const RHome = observer(
   }: {
     navigateAfterLogin?: () => void;
   }): React.ReactElement => {
+    const [data, setData] = useState<string>();
+    const [objectData, setObjectData] = useState<object>();
+
+    const storeData = async (value: string) => {
+      try {
+        await AsyncStorage.setItem('test-data', value);
+      } catch (e) {
+        // saving error
+        console.log('ERROR: ', e);
+      }
+    };
+
+    const storeObjectData = async (value: object) => {
+      try {
+        const jsonValue = JSON.stringify(value);
+        await AsyncStorage.setItem('test-object-data', jsonValue);
+      } catch (e) {
+        // saving error
+        console.log('ERROR: ', e);
+      }
+    };
+
+    const getData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('test-data');
+        if (value !== null) {
+          // value previously stored
+          setData(value);
+          return value;
+        }
+      } catch (e) {
+        // error reading value
+        console.log('ERROR: ', e);
+      }
+      return null;
+    };
+
+    const getObjectData = async () => {
+      try {
+        const jsonValue = await AsyncStorage.getItem('test-object-data');
+        // return jsonValue != null ? JSON.parse(jsonValue) : null;
+        const parsedValue = jsonValue ? JSON.parse(jsonValue) : null;
+        parsedValue && setObjectData(parsedValue);
+        return parsedValue;
+      } catch (e) {
+        // error reading value
+        console.log('ERROR: ', e);
+      }
+    };
+
+    useEffect(() => {
+      storeData('This be a test');
+      storeObjectData({ hello: 'I am a test' });
+      getData();
+      getObjectData();
+    }, []);
+
     return (
       <RAccessTemplate>
         <Text
@@ -17,8 +76,10 @@ export const RHome = observer(
             marginBottom: 30,
           }}
         >
-          Testing Login (also testing ReactHookForms and Backend Auth)
+          Testing Async Storage
         </Text>
+        <Text>{data}</Text>
+        <Text>{JSON.stringify(objectData)}</Text>
         <RFormLogin navigateAfterLogin={navigateAfterLogin} />
       </RAccessTemplate>
     );
