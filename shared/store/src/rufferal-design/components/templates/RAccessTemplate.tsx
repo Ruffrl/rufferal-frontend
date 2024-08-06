c513237781101422d003971734b38d47c0f191b4
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-08-06 11:51:08 -0500

    Add intellisense
    - https://marketplace.visualstudio.com/items?itemName=bradlc.vscode-tailwindcss
    - Must have a tailwind.config.js file
    - Within your VSCode user settings.json
    "tailwindCSS.classAttributes": [
        // ...
        "style"
      ],
      "tailwindCSS.experimental.classRegex": [
        "tw`([^`]*)",
        ["tw.style\\(([^)]*)\\)", "'([^']*)'"]
      ]

--- shared/store/src/rufferal-design/components/templates/RAccessTemplate.tsx
+++ shared/store/src/rufferal-design/components/templates/RAccessTemplate.tsx
@@ -6,7 +6,6 @@ import { ToastStoreContext } from '../../../store';
 import { MobileNavigation, WebNavigation } from '../../navigation';
 import { RFormError } from '../atom';
 
-// BLARG - update with this: https://reactnavigation.org/docs/navigating-without-navigation-prop and MobX
 export const RAccessTemplate = observer(
   ({ children }: React.PropsWithChildren): React.ReactElement => {
     const isMobile = Platform.OS === 'ios' || Platform.OS === 'android';
@@ -19,59 +18,20 @@ export const RAccessTemplate = observer(
     // );
 
     return (
-      // <View style={isMobile ? styles.containerMobile : styles.containerWeb}>
-      //   {toastStore.messages.length > 0 && (
-      //     <RFormError error={toastStore.messages.toString()} />
-      //   )}
-      //   <Text style={styles.welcome}>Rufferal</Text>
-      //   <Text style={styles.instructions}>Access Playground</Text>
-      //   <ScrollView>
-      //   {children}
-      //   </ScrollView>
-      //   {isMobile ? <MobileNavigation /> : <WebNavigation />}
-      // </View>
-      <View style={isMobile ? tw`bg-black` : tw`bg-orange-900`}>
-        {/* <View style={tw`ios:bg-black android:bg-black bg-orange-900`}> */}
+      <View
+        style={tw.style(
+          'my-4 mx-2 flex-1 justify-center items-center',
+          isMobile ? tw`bg-amber-400` : tw`bg-purple-400`
+        )}
+      >
         {toastStore.messages.length > 0 && (
           <RFormError error={toastStore.messages.toString()} />
         )}
-        {/* <Text style={styles.welcome}>Rufferal</Text> */}
-        <Text style={tw`text-2xl text-white`}>Rufferal</Text>
-        {/* <Text style={styles.instructions}>Access Playground</Text> */}
-        <Text style={tw`text-xl`}>Access Playground</Text>
+        <Text style={tw`text-2xl mb-4`}>Rufferal</Text>
+        <Text style={tw`text-xl mb-4`}>Access Playground</Text>
         <ScrollView>{children}</ScrollView>
         {isMobile ? <MobileNavigation /> : <WebNavigation />}
       </View>
     );
   }
 );
-
-// const styles = StyleSheet.create({
-//   containerWeb: {
-//     marginHorizontal: 4,
-//     marginVertical: 8,
-//     backgroundColor: 'aqua',
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   containerMobile: {
-//     marginHorizontal: 4,
-//     marginVertical: 8,
-//     width: '100%',
-//     height: '100%',
-//     backgroundColor: 'aqua',
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     marginBottom: 5,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 20,
-//   },
-// });
