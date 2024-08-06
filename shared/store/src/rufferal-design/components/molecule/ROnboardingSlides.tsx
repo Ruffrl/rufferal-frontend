3117c252c4d51101d50cc76e3756ae55e64ec5ba
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-08-06 17:38:43 -0500

    Convert tester to tailwind styles

--- shared/store/src/rufferal-design/components/molecule/ROnboardingSlides.tsx
+++ shared/store/src/rufferal-design/components/molecule/ROnboardingSlides.tsx
@@ -1,35 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
-// import tw from 'twrnc';
+import { Text, View } from 'react-native';
+import tw from 'twrnc';
+
+const CONTAINER_STYLE = 'flex-1 items-center justify-center';
 
 export const ROnboardingSlideOne = () => (
-  <View style={[styles.slideContainer, styles.slide1]}>
-    <Text>Slide 1</Text>
+  <View style={tw.style(CONTAINER_STYLE, 'bg-[#1414c84d]')}>
+    <Text>Welcome to Rufferal</Text>
   </View>
 );
 export const ROnboardingSlideTwo = () => (
-  <View style={[styles.slideContainer, styles.slide2]}>
-    <Text>Slide 2</Text>
+  <View style={tw.style(CONTAINER_STYLE, 'bg-[#14c8144d]')}>
+    <Text>Something is explained here</Text>
   </View>
 );
 export const ROnboardingSlideThree = () => (
-  <View style={[styles.slideContainer, styles.slide3]}>
-    <Text>Slide 3</Text>
+  <View style={tw.style(CONTAINER_STYLE, 'bg-[#c814144d]')}>
+    <Text>Cute little wrap up</Text>
   </View>
 );
-
-const styles = StyleSheet.create({
-  slideContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  slide1: {
-    backgroundColor: 'rgba(20,20,200,0.3)',
-  },
-  slide2: {
-    backgroundColor: 'rgba(20,200,20,0.3)',
-  },
-  slide3: {
-    backgroundColor: 'rgba(200,20,20,0.3)',
-  },
-});
