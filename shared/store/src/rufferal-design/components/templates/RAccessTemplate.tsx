d58641a9d1b19b8c5513515043a40d94182e057c
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-08-06 11:34:17 -0500

    Add and test a tailwind package
    - https://github.com/jaredh159/tailwind-react-native-classnames

--- shared/store/src/rufferal-design/components/templates/RAccessTemplate.tsx
+++ shared/store/src/rufferal-design/components/templates/RAccessTemplate.tsx
@@ -1,6 +1,7 @@
 import { observer } from 'mobx-react-lite';
 import { useContext } from 'react';
-import { Platform, StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Platform, ScrollView, Text, View } from 'react-native';
+import tw, { useDeviceContext } from 'twrnc';
 import { ToastStoreContext } from '../../../store';
 import { MobileNavigation, WebNavigation } from '../../navigation';
 import { RFormError } from '../atom';
@@ -9,6 +10,7 @@ import { RFormError } from '../atom';
 export const RAccessTemplate = observer(
   ({ children }: React.PropsWithChildren): React.ReactElement => {
     const isMobile = Platform.OS === 'ios' || Platform.OS === 'android';
+    useDeviceContext(tw);
     const toastStore = useContext(ToastStoreContext);
     // console.log('BLARG toastStore', toastStore);
     // console.log(
@@ -17,47 +19,59 @@ export const RAccessTemplate = observer(
     // );
 
     return (
-      <View style={isMobile ? styles.containerMobile : styles.containerWeb}>
+      // <View style={isMobile ? styles.containerMobile : styles.containerWeb}>
+      //   {toastStore.messages.length > 0 && (
+      //     <RFormError error={toastStore.messages.toString()} />
+      //   )}
+      //   <Text style={styles.welcome}>Rufferal</Text>
+      //   <Text style={styles.instructions}>Access Playground</Text>
+      //   <ScrollView>
+      //   {children}
+      //   </ScrollView>
+      //   {isMobile ? <MobileNavigation /> : <WebNavigation />}
+      // </View>
+      <View style={isMobile ? tw`bg-black` : tw`bg-orange-900`}>
+        {/* <View style={tw`ios:bg-black android:bg-black bg-orange-900`}> */}
         {toastStore.messages.length > 0 && (
           <RFormError error={toastStore.messages.toString()} />
         )}
-        <Text style={styles.welcome}>Rufferal</Text>
-        <Text style={styles.instructions}>Access Playground</Text>
-        <ScrollView>
-        {children}
-        </ScrollView>
+        {/* <Text style={styles.welcome}>Rufferal</Text> */}
+        <Text style={tw`text-2xl text-white`}>Rufferal</Text>
+        {/* <Text style={styles.instructions}>Access Playground</Text> */}
+        <Text style={tw`text-xl`}>Access Playground</Text>
+        <ScrollView>{children}</ScrollView>
         {isMobile ? <MobileNavigation /> : <WebNavigation />}
       </View>
     );
   }
 );
 
-const styles = StyleSheet.create({
-  containerWeb: {
-    marginHorizontal: 4,
-    marginVertical: 8,
-    backgroundColor: 'aqua',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  containerMobile: {
-    marginHorizontal: 4,
-    marginVertical: 8,
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'aqua',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginBottom: 5,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 20,
-  },
-});
+// const styles = StyleSheet.create({
+//   containerWeb: {
+//     marginHorizontal: 4,
+//     marginVertical: 8,
+//     backgroundColor: 'aqua',
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   containerMobile: {
+//     marginHorizontal: 4,
+//     marginVertical: 8,
+//     width: '100%',
+//     height: '100%',
+//     backgroundColor: 'aqua',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     marginBottom: 5,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 20,
+//   },
+// });
