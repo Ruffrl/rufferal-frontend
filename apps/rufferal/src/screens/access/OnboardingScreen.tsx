ac8b005a6ee54a88cbec5b5bf2733098dd60b4e2
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-08-06 16:49:21 -0500

    Add and test `react-native-web-swiper`
    - `npm i react-native-web-swiper --save`
    - https://github.com/reactrondev/react-native-web-swiper#readme

--- apps/rufferal/src/screens/access/OnboardingScreen.tsx
+++ apps/rufferal/src/screens/access/OnboardingScreen.tsx
@@ -2,5 +2,5 @@ import { ROnboarding } from '@rufferal-frontend/store';
 import { observer } from 'mobx-react-lite';
 
 export const OnboardingScreen = observer(() => {
-  return <ROnboarding />;
+  return <ROnboarding mobile />;
 });
--- shared/store/src/rufferal-design/components/pages/access/ROnboarding.tsx
+++ shared/store/src/rufferal-design/components/pages/access/ROnboarding.tsx
@@ -1,16 +1,70 @@
 import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Swiper from 'react-native-web-swiper';
 
-import { Text, View } from 'react-native';
-import tw from 'twrnc';
-import { observableAuthStore } from '../../../../store';
-import { RAccessTemplate } from '../../templates';
+// import tw from 'twrnc';
+// import { observableAuthStore } from '../../../../store';
+// import { RAccessTemplate } from '../../templates';
 
-export const ROnboarding = (): React.ReactElement => {
+export const ROnboarding = ({
+  mobile = false,
+}: {
+  mobile?: boolean;
+  }): React.ReactElement => {
+  const SlideOne = () => (
+    <View style={[styles.slideContainer, styles.slide1]}>
+      <Text>Slide 1</Text>
+    </View>
+  );
+  const SlideTwo = () => (
+    <View style={[styles.slideContainer, styles.slide2]}>
+      <Text>Slide 2</Text>
+    </View>
+  );
+  const SlideThree = () => (
+    <View style={[styles.slideContainer, styles.slide3]}>
+      <Text>Slide 3</Text>
+    </View>
+  );
+
+  const MobileSwiper = () => (
+    <>
+      <SlideOne />
+      <SlideTwo />
+      <SlideThree />
+    </>
+  );
+  const WebSwiper = () => (
+    <Swiper>
+      <SlideOne />
+      <SlideTwo />
+      <SlideThree />
+    </Swiper>
+  );
+
   return (
-    <RAccessTemplate>
-      <View style={tw`mt-[30px] mb-[30px]`}>
-        <Text>{observableAuthStore.user?.id}</Text>
-      </View>
-    </RAccessTemplate>
+    <View style={styles.container}>
+      {mobile ? <MobileSwiper /> : <WebSwiper />}
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  slideContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  slide1: {
+    backgroundColor: 'rgba(20,20,200,0.3)',
+  },
+  slide2: {
+    backgroundColor: 'rgba(20,200,20,0.3)',
+  },
+  slide3: {
+    backgroundColor: 'rgba(200,20,20,0.3)',
+  },
+});
