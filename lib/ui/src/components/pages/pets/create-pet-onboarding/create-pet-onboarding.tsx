3e9d4e3035851d7377d998f2cd21c83ec882efa6
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-09-16 17:52:55 -0500

    Test assets

--- lib/ui/src/components/pages/pets/create-pet-onboarding/create-pet-onboarding.tsx
+++ lib/ui/src/components/pages/pets/create-pet-onboarding/create-pet-onboarding.tsx
@@ -1,3 +1,4 @@
+import { Image } from 'expo-image';
 import { Text, View } from 'react-native';
 
 /* eslint-disable-next-line */
@@ -7,6 +8,13 @@ export const CreatePetOnboarding = (props: CreatePetOnboardingProps) => {
   return (
     <View>
       <Text>Welcome to create-pet-onboarding!</Text>
+      <Image
+        style={{
+          width: 50,
+          height: 50,
+        }}
+        source={require('@rufferal/assets/images/husky-and-orange-cat.png')}
+      />
     </View>
   );
 };
