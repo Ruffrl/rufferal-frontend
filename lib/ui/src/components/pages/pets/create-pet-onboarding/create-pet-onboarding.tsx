8f811edf45fce9f9835a791772b01335bddef36a
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-09-17 09:24:01 -0500

    Setup customization for tailwind and device dimension handling

--- lib/ui/src/components/pages/pets/create-pet-onboarding/create-pet-onboarding.tsx
+++ lib/ui/src/components/pages/pets/create-pet-onboarding/create-pet-onboarding.tsx
@@ -1,4 +1,9 @@
-import { Image } from 'expo-image';
+// import React from 'react';
+// import tw from 'twrnc';
+// BLARG - is there a cleaner way to do this? Should I build an NX library/module for my tailwind customization? Then I can import as @rufferal/tailwind or something?
+import tw from '../../../../../tailwind';
+
+import * as expoImage from 'expo-image';
 import { Text, View } from 'react-native';
 
 /* eslint-disable-next-line */
@@ -6,13 +11,11 @@ export interface CreatePetOnboardingProps {}
 
 export const CreatePetOnboarding = (props: CreatePetOnboardingProps) => {
   return (
-    <View>
+    // <View style={tw`bg-[#A0EFFE]`}>
+    <View style={tw`bg-blizzardBlue-200`}>
       <Text>Welcome to create-pet-onboarding!</Text>
-      <Image
-        style={{
-          width: 50,
-          height: 50,
-        }}
+      <expoImage.Image
+        style={tw`w-48 h-48`}
         source={require('@rufferal/assets/images/husky-and-orange-cat.png')}
       />
     </View>
---
+++ lib/ui/src/utils/index.ts
@@ -0,0 +1,24 @@
+export {
+  horizontalScale,
+  horizontalScaleTW,
+  moderateScale,
+  moderateScaleTW,
+  verticalScale,
+  verticalScaleTW,
+} from './dimensions';
+
+export {
+  GLOBAL_ANDROID_SHADOW,
+  GLOBAL_COLORS,
+  GLOBAL_ICON_SIZE,
+  GLOBAL_IOS_SHADOW,
+  GLOBAL_MIN_PRESS_HEIGHT,
+  GLOBAL_MIN_PRESS_SIZE,
+  GLOBAL_PROTOTYPE_COLORS,
+  GLOBAL_SMALL_ICON_SIZE,
+  GLOBAL_WEB_SHADOW,
+  GLOBAL_X_PADDING,
+  GLOBAL_Y_PADDING,
+} from './css-globals';
+
+export { capitalize } from './capitalize';
