b2cd75ff24a62689e4ff0e1f17b2331d4f05dae9
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-09-19 14:52:12 -0500

    Fix padding on navigation

--- ui/src/components/organisms/page-navigation/page-navigation.tsx
+++ ui/src/components/organisms/page-navigation/page-navigation.tsx
@@ -1,5 +1,5 @@
 import { ruffwind } from '@rufferal/tailwind';
-import { GLOBAL_ICON_SIZE, GLOBAL_MIN_PRESS_SIZE } from '@rufferal/utils';
+import { GLOBAL_ICON_SIZE } from '@rufferal/utils';
 import { Image } from 'expo-image';
 import { Pressable, Text, View } from 'react-native';
 
@@ -10,44 +10,32 @@ export const PageNavigation = ({
   backNavigation: () => void;
   skipNavigation?: () => void;
 }) => {
+  const containerStyles = ruffwind`justify-center`;
+  const buttonStyles = ruffwind`flex-row items-center gap-1`;
+  const iconStyles = ruffwind.style(
+    GLOBAL_ICON_SIZE,
+    'items-center justify-center'
+  );
+  const textStyles = ruffwind`font-bodySemibold text-electricViolet-800 text-b3`;
+
   return (
     <View style={ruffwind`w-full flex-row justify-between`}>
-      <Pressable style={ruffwind`justify-center`} onPress={backNavigation}>
-        <View style={ruffwind`flex-row items-center gap-1`}>
+      <Pressable style={containerStyles} onPress={backNavigation}>
+        <View style={buttonStyles}>
           <Image
             source={require('@rufferal/assets/src/icons/chevron-left.png')}
-            style={ruffwind.style(
-              GLOBAL_ICON_SIZE,
-              'items-center justify-center'
-            )}
+            style={iconStyles}
           />
-          <Text
-            style={ruffwind`font-bodySemibold text-electricViolet-800 text-b3`}
-          >
-            Back
-          </Text>
+          <Text style={textStyles}>Back</Text>
         </View>
       </Pressable>
       {skipNavigation && (
-        <Pressable
-          style={ruffwind.style(
-            GLOBAL_MIN_PRESS_SIZE,
-            ruffwind`justify-center`
-          )}
-          onPress={skipNavigation}
-        >
-          <View style={ruffwind`flex-row items-center gap-1`}>
-            <Text
-              style={ruffwind`font-bodySemibold text-electricViolet-800 text-b3`}
-            >
-              Skip
-            </Text>
+        <Pressable style={containerStyles} onPress={skipNavigation}>
+          <View style={buttonStyles}>
+            <Text style={textStyles}>Skip</Text>
             <Image
-              source={require('@rufferal/assets/src/icons/chevron-left.png')}
-              style={ruffwind.style(
-                GLOBAL_ICON_SIZE,
-                'items-center justify-center'
-              )}
+              source={require('@rufferal/assets/src/icons/chevron-right.png')}
+              style={iconStyles}
             />
           </View>
         </Pressable>
