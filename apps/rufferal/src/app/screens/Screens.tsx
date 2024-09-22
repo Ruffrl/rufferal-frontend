c7f5133553dfc27d37018f4db8a1d5d362313aaf
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-09-22 12:07:44 -0500

    Copy to dog personality form

--- apps/rufferal/src/app/screens/Screens.tsx
+++ apps/rufferal/src/app/screens/Screens.tsx
@@ -23,7 +23,7 @@ export const Screens = observer(() => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {/* ⬇️⬇️⬇️ CURRENT DEVELOPMENT PAGE ⬇️⬇️⬇️ */}
-        <Stack.Screen name="Cat Personality" component={CatPersonalityScreen} />
+        <Stack.Screen name="Dog Personality" component={DogPersonalityScreen} />
         {/* ⬆️⬆️⬆️ CURRENT DEVELOPMENT PAGE ⬆️⬆️⬆️ */}
 
         <Stack.Screen
@@ -33,163 +33,16 @@ export const Screens = observer(() => {
         <Stack.Screen name="Manage Pets" component={ManagePetsScreen} />
         <Stack.Screen name="Cat Details" component={CatDetailsScreen} />
         <Stack.Screen name="Cat Avatar" component={CatAvatarScreen} />
-        {/* <Stack.Screen name="Cat Personality" component={CatPersonalityScreen} /> */}
+        <Stack.Screen name="Cat Personality" component={CatPersonalityScreen} />
         <Stack.Screen name="Cat Careplan" component={CatCareplanScreen} />
         <Stack.Screen name="Dog Details" component={DogDetailsScreen} />
         <Stack.Screen name="Dog Avatar" component={DogAvatarScreen} />
-        <Stack.Screen name="Dog Personality" component={DogPersonalityScreen} />
+        {/* <Stack.Screen name="Dog Personality" component={DogPersonalityScreen} /> */}
         <Stack.Screen name="Dog Careplan" component={DogCareplanScreen} />
         <Stack.Screen name="Search" component={SearchScreen} />
-        
+
         <Stack.Screen name="Camera" component={CameraScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 });
-
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import { observableAccountStore } from '@rufferal/store';
-// import { Bottomsheet } from '@rufferal/ui';
-// import { observer } from 'mobx-react-lite';
-// import { useEffect, useState } from 'react';
-// import {
-//   CreatePetCareplanScreen,
-//   CreatePetDetailsScreen,
-//   CreatePetPersonalityScreen,
-//   CreateProfileAvatarScreen,
-//   CreateProfileNameScreen,
-//   CreateProfilePetsScreen,
-//   CreateProfileTypeScreen,
-//   Dashboard2,
-//   DashboardScreen,
-//   DevPlaygroundScreen,
-//   ForgotPasswordScreen,
-//   LoginScreen,
-//   ManagePetsScreen,
-//   OnboardingScreen,
-//   ProfileScreen,
-//   ResetPasswordScreen,
-//   SignupScreen,
-//   VerificationScreen,
-// } from '.';
-
-// function CatScreen() {
-//   return <Bottomsheet />;
-// }
-
-// const Stack = createNativeStackNavigator();
-
-// export const Screens = observer(() => {
-//   // showOnboarding == true -> they have not interacted with Onboarding, do display
-//   // showOnboarding == false -> they have interacted with Onboarding, do NOT display again
-//   // showOnboarding will check AsyncStorage for key "showOnboarding"
-//   // If a user hasn't interacted with Onboarding, getItem("showOnboarding") should be null
-//   // If a user interacts with Onboarding, we setItem("showOnboarding", "true")
-//   // At the Screens level, we just get AsyncStorage
-
-//   /* BLARG - DEVELOPING/DEBUGGING */
-//   const [showOnboarding, setShowOnboarding] = useState<boolean>(true);
-
-//   useEffect(() => {
-//     AsyncStorage.getItem('showOnboarding').then((value) => {
-//       if (value === 'true') {
-//         setShowOnboarding(false);
-//       }
-//     });
-//   });
-
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator screenOptions={{ headerShown: false }}>
-//         <Stack.Screen name="Cat Facts" component={CatScreen} />
-//         {observableAccountStore.isLoggedIn ? (
-//           /* USER SCREENS */
-//           <Stack.Group>
-//             <Stack.Screen name="Dashboard" component={DashboardScreen} />
-//             {/* BLARG handle conditionally rendering create profile */}
-//             <Stack.Screen
-//               name="Create Profile Name"
-//               component={CreateProfileNameScreen}
-//             />
-//             <Stack.Screen
-//               name="Create Profile Avatar"
-//               component={CreateProfileAvatarScreen}
-//             />
-//             <Stack.Screen
-//               name="Create Profile Type"
-//               component={CreateProfileTypeScreen}
-//             />
-//             <Stack.Screen
-//               name="Create Profile Pets"
-//               component={CreateProfilePetsScreen}
-//             />
-//             <Stack.Screen name="Manage Pets" component={ManagePetsScreen} />
-//             <Stack.Screen
-//               name="Create Pet Details"
-//               component={CreatePetDetailsScreen}
-//             />
-//             <Stack.Screen
-//               name="Create Pet Personality"
-//               component={CreatePetPersonalityScreen}
-//             />
-//             <Stack.Screen
-//               name="Create Pet Careplan"
-//               component={CreatePetCareplanScreen}
-//             />
-//             {/* BLARG - screens tbd */}
-//             <Stack.Screen name="Dashboard2" component={Dashboard2} />
-//             <Stack.Screen name="Profile" component={ProfileScreen} />
-//             {/* <Stack.Screen name="Messaging" component={MessagingScreen} /> */}
-//             {/* <Stack.Screen name="Bookings" component={BookingsScreen} /> */}
-//             {/* <Stack.Screen name="AccountSettings" component={AccountSettingsScreen} /> */}
-//           </Stack.Group>
-//         ) : (
-//           /* GUEST SCREENS */
-//           <Stack.Group>
-//             {/* ⬇️⬇️⬇️ BLARG - FOR DEVELOPMENT - REMOVE AFTER TESTING ⬇️⬇️⬇️ */}
-//             <Stack.Screen name="Manage Pets" component={ManagePetsScreen} />
-//             <Stack.Screen
-//               name="Create Pet Details"
-//               component={CreatePetDetailsScreen}
-//             />
-//             <Stack.Screen
-//               name="Create Pet Personality"
-//               component={CreatePetPersonalityScreen}
-//             />
-//             <Stack.Screen
-//               name="Create Pet Careplan"
-//               component={CreatePetCareplanScreen}
-//             />
-//             {/* ⬆️⬆️⬆️ BLARG - FOR DEVELOPMENT - REMOVE AFTER TESTING ⬆️⬆️⬆️ */}
-//             {showOnboarding && (
-//               <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-//             )}
-//             <Stack.Screen name="Login" component={LoginScreen} />
-//             <Stack.Screen name="Verification" component={VerificationScreen} />
-//             <Stack.Screen
-//               name="Forgot Password"
-//               component={ForgotPasswordScreen}
-//             />
-//             <Stack.Screen
-//               name="Reset Password"
-//               component={ResetPasswordScreen}
-//             />
-//             <Stack.Screen name="Signup" component={SignupScreen} />
-//             <Stack.Screen name="Dashboard" component={DashboardScreen} />
-//           </Stack.Group>
-//         )}
-//         {/* SHARED SCREENS */}
-//         <Stack.Group
-//           navigationKey={observableAccountStore.isLoggedIn ? 'user' : 'guest'}
-//         >
-//           {/* BLARG - todo */}
-//           <Stack.Screen name="About" component={DevPlaygroundScreen} />
-//           {/* BLARG - todo */}
-//           <Stack.Screen name="Help" component={DevPlaygroundScreen} />
-//         </Stack.Group>
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// });
--- ui/src/components/pages/pets/create/dogs/dog-personality/dog-personality.tsx
+++ ui/src/components/pages/pets/create/dogs/dog-personality/dog-personality.tsx
@@ -4,7 +4,7 @@ import {
   verticalScaleTW,
 } from '@rufferal/utils';
 import { Image } from 'expo-image';
-import { ScrollView, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 import { useState } from 'react';
 import { PageNavigationProps } from '../../../..';
@@ -17,7 +17,7 @@ import {
   Tag,
   Toggle,
 } from '../../../../../atoms';
-import { FeatureTemplate } from '../../../../../templates';
+import { ScrollFeatureTemplate } from '../../../../../templates/scroll-feature-template/scroll-feature-template';
 
 /* eslint-disable-next-line */
 export interface DogPersonalityProps extends PageNavigationProps {}
@@ -25,6 +25,7 @@ export interface DogPersonalityProps extends PageNavigationProps {}
 export const DogPersonality = ({ navigation }: DogPersonalityProps) => {
   const [goodWithKids, setGoodWithKids] = useState(false);
   const handleGoodWithKids = () => setGoodWithKids((prev) => !prev);
+  // useCallback(() => { }, [])
 
   const [goodWithCats, setGoodWithCats] = useState(false);
   const handleGoodWithCats = () => setGoodWithCats((prev) => !prev);
@@ -32,8 +33,8 @@ export const DogPersonality = ({ navigation }: DogPersonalityProps) => {
   const [goodWithDogs, setGoodWithDogs] = useState(false);
   const handleGoodWithDogs = () => setGoodWithDogs((prev) => !prev);
 
-  const [careDeclawed, setCareDeclawed] = useState(false);
-  const handleCareDeclawed = () => setCareDeclawed((prev) => !prev);
+  const [careHousetrained, setCareHousetrained] = useState(false);
+  const handleCareHousetrained = () => setCareHousetrained((prev) => !prev);
 
   const [careMedication, setCareMedication] = useState(false);
   const handleCareMedication = () => setCareMedication((prev) => !prev);
@@ -56,156 +57,157 @@ export const DogPersonality = ({ navigation }: DogPersonalityProps) => {
   const [personalityMotivation, setPersonalityMotivation] = useState<number>();
   const handleMotivation = (value: number) => setPersonalityMotivation(value);
 
-  const BehaviorLabel = ({
-    label,
-    multiple = false,
-  }: {
-    label: string;
-    multiple?: boolean;
-  }) => (
-    <View style={ruffwind`flex-row justify-between`}>
-      <Text
-        style={ruffwind`font-bodySemibold text-b3 text-left text-balticSea-950`}
-      >
-        {label}
-      </Text>
-      {multiple && (
-        <Text style={ruffwind`font-body text-b2 text-right text-saltBox-700`}>
-          Select multiple
-        </Text>
-      )}
-    </View>
-  );
-
-  const ToggleLabel = ({ label }: { label: string }) => (
-    <Text style={ruffwind`font-body text-b3 text-saltBox-700`}>{label}</Text>
-  );
-
   return (
-    <FeatureTemplate
+    <ScrollFeatureTemplate
       backNavigation={() => navigation.navigate('Dog Avatar')}
       skipNavigation={() => navigation.navigate('Dog Careplan')}
     >
-      <ScrollView>
-        <View style={ruffwind`mt-6`}>
-          <ProgressBar step={3} total={4} />
-        </View>
-        <View style={ruffwind`mt-6 gap-2`}>
-          <Tag
-            Icon={() => (
-              <Image
-                style={ruffwind.style(
-                  GLOBAL_ICON_SIZE_MEDIUM_SMALL,
-                  'items-center justify-center'
-                )}
-                source={require('@rufferal/assets/src/icons/dog.png')}
-              />
-            )}
-            text="Maya"
-          />
-          <H3 text="Behavior and personality" />
-          <Text style={ruffwind`text-balticSea-950 font-body text-b2`}>
-            Help pet caretakers understand your dog’s needs and unique traits
-          </Text>
-        </View>
-        <View style={ruffwind`mt-5 gap-4`}>
-          <View style={ruffwind`gap-2`}>
-            <BehaviorLabel label="Good with" multiple />
-            <View style={ruffwind`justify-between flex-row`}>
-              <ToggleLabel label="Kids" />
-              <Toggle
-                toggleState={goodWithKids}
-                setToggleState={handleGoodWithKids}
-              />
-            </View>
-            <View style={ruffwind`justify-between flex-row`}>
-              <ToggleLabel label="Cats" />
-              <Toggle
-                toggleState={goodWithCats}
-                setToggleState={handleGoodWithCats}
-              />
-            </View>
-            <View style={ruffwind`justify-between flex-row`}>
-              <ToggleLabel label="Other dogs" />
-              <Toggle
-                toggleState={goodWithDogs}
-                setToggleState={handleGoodWithDogs}
-              />
-            </View>
+      <View style={ruffwind`mt-6`}>
+        <ProgressBar step={3} total={4} />
+      </View>
+      <View style={ruffwind`mt-6 gap-2`}>
+        <Tag
+          Icon={() => (
+            <Image
+              style={ruffwind.style(
+                GLOBAL_ICON_SIZE_MEDIUM_SMALL,
+                'items-center justify-center'
+              )}
+              source={require('@rufferal/assets/src/icons/dog.png')}
+            />
+          )}
+          text="Maya"
+        />
+        <H3 text="Behavior and personality" />
+        <Text style={ruffwind`text-balticSea-950 font-body text-b2`}>
+          Help pet caretakers understand your dog’s needs and unique traits
+        </Text>
+      </View>
+      <View style={ruffwind`mt-5 gap-4`}>
+        <View style={ruffwind`gap-2`}>
+          <BehaviorLabel label="Good with" multiple />
+          <View style={ruffwind`justify-between flex-row`}>
+            <ToggleLabel label="Kids" />
+            <Toggle
+              toggleState={goodWithKids}
+              setToggleState={handleGoodWithKids}
+            />
           </View>
-          <HorizontalDivider color="border-saltBox-200" />
-          <View>
-            <BehaviorLabel label="Personality" />
-            <View style={ruffwind`mt-1 gap-3`}>
-              <SingleSlider
-                handleChange={handleTemperment}
-                labels={['shy', 'social']}
-                value={personalityTemperment}
-              />
-              <SingleSlider
-                handleChange={handleEnergy}
-                labels={['relaxed', 'active']}
-                value={personalityEnergy}
-              />
-              <SingleSlider
-                handleChange={handleAutonomy}
-                labels={['dependent', 'independent']}
-                value={personalityAutonomy}
-              />
-              <SingleSlider
-                handleChange={handleMotivation}
-                labels={['food motivated', 'praise motivated']}
-                value={personalityMotivation}
-              />
-            </View>
+          <View style={ruffwind`justify-between flex-row`}>
+            <ToggleLabel label="Cats" />
+            <Toggle
+              toggleState={goodWithCats}
+              setToggleState={handleGoodWithCats}
+            />
           </View>
-          <HorizontalDivider color="border-saltBox-200" />
-          <View style={ruffwind`gap-2`}>
-            <BehaviorLabel label="Care & behavior" multiple />
-            <View style={ruffwind`justify-between flex-row`}>
-              <ToggleLabel label="Declawed" />
-              <Toggle
-                toggleState={careDeclawed}
-                setToggleState={handleCareDeclawed}
-              />
-            </View>
-            <View style={ruffwind`justify-between flex-row`}>
-              <ToggleLabel label="Requires medication" />
-              <Toggle
-                toggleState={careMedication}
-                setToggleState={handleCareMedication}
-              />
-            </View>
-            <View style={ruffwind`justify-between flex-row`}>
-              <ToggleLabel label="Separation anxiety" />
-              <Toggle
-                toggleState={careAnxiety}
-                setToggleState={handleCareAnxiety}
-              />
-            </View>
-            <View style={ruffwind`justify-between flex-row`}>
-              <ToggleLabel label="Special needs" />
-              <Toggle
-                toggleState={careSpecial}
-                setToggleState={handleCareSpecial}
-              />
-            </View>
+          <View style={ruffwind`justify-between flex-row`}>
+            <ToggleLabel label="Other dogs" />
+            <Toggle
+              toggleState={goodWithDogs}
+              setToggleState={handleGoodWithDogs}
+            />
           </View>
         </View>
-        <View style={ruffwind.style(`gap-2 mt-${verticalScaleTW(37)}`)}>
-          <HorizontalDivider color="border-amethystSmoke-600" />
-          <Button
-            text="Next"
-            onPress={() => navigation.navigate('Dog Careplan')}
-          />
-          <Button
-            text="Cancel"
-            type="transparent"
-            size="standard-short"
-            onPress={() => navigation.navigate('Manage Pets')}
-          />
+        <HorizontalDivider color="border-saltBox-200" />
+        <View>
+          <BehaviorLabel label="Personality" />
+          <View style={ruffwind`mt-1 gap-3`}>
+            <SingleSlider
+              handleChange={handleTemperment}
+              labels={['shy', 'social']}
+              value={personalityTemperment}
+            />
+            <SingleSlider
+              handleChange={handleEnergy}
+              labels={['relaxed', 'active']}
+              value={personalityEnergy}
+            />
+            <SingleSlider
+              handleChange={handleAutonomy}
+              labels={['dependent', 'independent']}
+              value={personalityAutonomy}
+            />
+            <SingleSlider
+              handleChange={handleMotivation}
+              labels={['food motivated', 'praise motivated']}
+              value={personalityMotivation}
+            />
+          </View>
         </View>
-      </ScrollView>
-    </FeatureTemplate>
+        <HorizontalDivider color="border-saltBox-200" />
+        <View style={ruffwind`gap-2`}>
+          <BehaviorLabel label="Care & behavior" multiple />
+          <View style={ruffwind`justify-between flex-row`}>
+            <ToggleLabel label="House trained" />
+            <Toggle
+              toggleState={careHousetrained}
+              setToggleState={handleCareHousetrained}
+            />
+          </View>
+          <View style={ruffwind`justify-between flex-row`}>
+            <ToggleLabel label="Requires medication" />
+            <Toggle
+              toggleState={careMedication}
+              setToggleState={handleCareMedication}
+            />
+          </View>
+          <View style={ruffwind`justify-between flex-row`}>
+            <ToggleLabel label="Separation anxiety" />
+            <Toggle
+              toggleState={careAnxiety}
+              setToggleState={handleCareAnxiety}
+            />
+          </View>
+          <View style={ruffwind`justify-between flex-row`}>
+            <ToggleLabel label="Special needs" />
+            <Toggle
+              toggleState={careSpecial}
+              setToggleState={handleCareSpecial}
+            />
+          </View>
+        </View>
+      </View>
+      <View style={ruffwind.style(`gap-2 mt-${verticalScaleTW(37)}`)}>
+        <HorizontalDivider color="border-amethystSmoke-600" />
+        <Button
+          text="Next"
+          onPress={() => navigation.navigate('Dog Careplan')}
+        />
+        <Button
+          text="Cancel"
+          type="transparent"
+          size="standard-short"
+          onPress={() => navigation.navigate('Manage Pets')}
+        />
+      </View>
+    </ScrollFeatureTemplate>
   );
 };
+
+/* ********** */
+/* COMPONENTS */
+/* ********** */
+const BehaviorLabel = ({
+  label,
+  multiple = false,
+}: {
+  label: string;
+  multiple?: boolean;
+}) => (
+  <View style={ruffwind`flex-row justify-between`}>
+    <Text
+      style={ruffwind`font-bodySemibold text-b3 text-left text-balticSea-950`}
+    >
+      {label}
+    </Text>
+    {multiple && (
+      <Text style={ruffwind`font-body text-b2 text-right text-saltBox-700`}>
+        Select multiple
+      </Text>
+    )}
+  </View>
+);
+
+const ToggleLabel = ({ label }: { label: string }) => (
+  <Text style={ruffwind`font-body text-b3 text-saltBox-700`}>{label}</Text>
+);
