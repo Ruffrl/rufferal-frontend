b21af370c6554be47e741b44864091cfb9248596
Priya Power <49959312+priyapower@users.noreply.github.com>
2024-09-22 12:21:53 -0500

    Setup base for careplan

--- ui/src/components/pages/pets/create/cats/cat-careplan/cat-careplan.tsx
+++ ui/src/components/pages/pets/create/cats/cat-careplan/cat-careplan.tsx
@@ -1,13 +1,44 @@
+import { ruffwind } from '@rufferal/tailwind';
+import { GLOBAL_ICON_SIZE_MEDIUM_SMALL } from '@rufferal/utils';
+import { Image } from 'expo-image';
 import { Text, View } from 'react-native';
 import { PageNavigationProps } from '../../../..';
+import { H3, ProgressBar, Tag } from '../../../../../atoms';
+import { ScrollFeatureTemplate } from '../../../../../templates';
 
 /* eslint-disable-next-line */
 export interface CatCareplanProps extends PageNavigationProps {}
 
 export const CatCareplan = ({ navigation }: CatCareplanProps) => {
   return (
-    <View>
-      <Text>Welcome to cat - create careplan!</Text>
-    </View>
+    <ScrollFeatureTemplate>
+      <View style={ruffwind`mt-6`}>
+        <ProgressBar step={3} total={4} />
+      </View>
+      <View style={ruffwind`mt-6 gap-2`}>
+        <Tag
+          Icon={() => (
+            <Image
+              style={ruffwind.style(
+                GLOBAL_ICON_SIZE_MEDIUM_SMALL,
+                'items-center justify-center'
+              )}
+              source={require('@rufferal/assets/src/icons/cat.png')}
+            />
+          )}
+          text="Gavin"
+        />
+        <H3 text="Add a careplan" />
+        <Text style={ruffwind`text-balticSea-950 font-body text-b2`}>
+          Turn toggle on to include instructions in this pet’s bookings
+        </Text>
+      </View>
+      <View style={ruffwind`mt-6 gap-2`}>
+        <View>
+          <Text>Harness</Text>
+          <Text>Toggle</Text>
+        </View>
+      </View>
+    </ScrollFeatureTemplate>
   );
 };
